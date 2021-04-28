7ff417342916b1c75a0ac2c5373f8e71d7d8bf13
Asra Siddiqui <asra@o-que.com>
2021-04-28 09:57:21 -0700

    change email address on contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,7 +3,7 @@
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
 
-  $email_from = 'asrasiddiqui127@gmail.com';
+  $email_from = 'converttosmartobject@gmail.com';
 
   $email_subject = "New Contact Form Submission"
 
